6d2b9e4077f8032cfc2c7c2ce736bb96eeef3e77
altbdoor <lancersupraskyline@gmail.com>
2014-02-26 18:45:08 +0800

    downgraded jquery to support IE8 and above, basic dropdown completed

---
+++ plugin.js
@@ -0,0 +1,76 @@
+/* enclosed to prevent namespace pollution */
+(function () {
+	/* setup
+	==================================================*/
+	if (typeof(psSetup) !== 'undefined' && typeof(psSetup) === 'object') {
+		if (psSetup.dropdownAnimation) {
+			
+		}
+	}
+	
+	/* dropdown
+	==================================================*/
+	var allMenu = $('.dropdown-menu');
+	
+	$('.dropdown').each(function () {
+		var mode = $(this).data('dropdown-trigger'),
+			anim = $(this).data('dropdown-animation'),
+			btn = $(this).children('button, a').first(),
+			menu = $(this).children('.dropdown-menu').first();
+		
+		if (mode == 'hover' || mode == 'mouseover') {
+			var hoverState = false,
+				hoverTimer;
+			
+			$(this).on('mouseenter', function () {
+				hoverState = true;
+				
+				$(allMenu).not(menu).hide();
+				
+				if (anim == 'fade') {
+					$(menu).stop(true, true).fadeIn();
+				}
+				else if (anim == 'slide') {
+					$(menu).stop(true, true).slideDown();
+				}
+				else {
+					$(menu).show();
+				}
+			}).on('mouseleave', function () {
+				hoverState = false;
+				clearTimeout(hoverTimer);
+				hoverTimer = setTimeout(function () {
+					if (!hoverState) {
+						if (anim == 'fade') {
+							$(menu).stop(true, true).fadeOut();
+						}
+						else if (anim == 'slide') {
+							$(menu).stop(true, true).slideUp();
+						}
+						else {
+							$(menu).hide();
+						}
+					}
+				}, 200);
+			});
+		}
+		else {
+			$(btn).on('click', function () {
+				$(allMenu).not(menu).hide();
+				
+				if (anim == 'fade') {
+					$(menu).stop(true, true).fadeToggle();
+				}
+				else if (anim == 'slide') {
+					$(menu).stop(true, true).slideToggle();
+				}
+				else {
+					$(menu).toggle();
+				}
+			});
+		}
+	});
+	
+	/* 
+	==================================================*/
+})();
